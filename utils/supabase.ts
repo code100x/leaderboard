ce40f08957e990258163b293fc1551b104c5395b
Arpit Khandelwal <arpit.khandelwal2002@gmail.com>
2024-08-29 08:47:46 +0530

    env

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://vibbbkpdgsocdulyjfpo.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.SUPABASE_KEY ||""
 
-export const supabase = createClient(supabaseUrl, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpYmJia3BkZ3NvY2R1bHlqZnBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4ODQ1MTYsImV4cCI6MjA0MDQ2MDUxNn0.lNFpKgqdaQDw9kAG1t4uWLxUngA1S8ezJ443h6Yg-28")
+export const supabase = createClient(supabaseUrl, supabaseKey)
