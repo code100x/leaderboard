34a0e9ef237d16db21b8f8a9052e2cf7e168dffc
Arpit Khandelwal <arpit.khandelwal2002@gmail.com>
2024-09-11 01:19:41 +0530

    add cron for refresh contributors

--- app/api/refreshDB/route.ts
+++ app/api/refreshDB/route.ts
@@ -5,23 +5,34 @@ import { supabase } from "@/utils/supabase"
 
 export const GET = async (req: any, res: any) => {
 
-    const users = await fetchUsers();
+    let contributorsData: { username: string; totalPRs: number; mergedPRs: number; openPRs: number; issues: number; avatar?: string }[] = []
+
+    cron.schedule('* * * * *', async () => {
+
+        console.log('')
+        console.log('#########################################')
+        console.log('#                                       #')
+        console.log('# Refreshing contributions every minute #')
+        console.log('#                                       #')
+        console.log('#########################################')
+        console.log('')
+        const users = await fetchUsers();
 
 
-    const usernames = users?.map((user: any) => user.username) || [];
+        const usernames = users?.map((user: any) => user.username) || [];
 
-    console.log("usernames:", usernames);
-    let contributorsData = []
+        console.log("usernames:", usernames);
 
-    for (let i = 0; i < usernames.length; i++) {
-        contributorsData.push(await fetchContributionData(usernames[i]));
-    }
+        for (let i = 0; i < usernames.length; i++) {
+            contributorsData.push(await fetchContributionData(usernames[i]));
+        }
 
-    console.log("contributorsData:", contributorsData);
+        console.log("contributorsData:", contributorsData);
 
-    await supabase.from('contributions').upsert(contributorsData);
-
-
+        const updatedContributions = await supabase.from('contributions').upsert(contributorsData);
+        if (updatedContributions.error)
+            console.error("updatedContributions error:", updatedContributions.error)
+    });
     return Response.json(contributorsData);
 }
 
@@ -36,4 +47,27 @@ const fetchUsers = async () => {
 
     if (error) { console.log('error', error); return [] }
     return Users;
+}
+
+import { NextResponse } from "next/server";
+
+import cron from 'node-cron';
+
+export async function POST(req: any, res: any) {
+
+    try {
+
+
+
+        await fetch('/api/refreshDB', { method: "GET" });
+
+
+
+        return NextResponse.json({ data: 'Success', status: 200 });
+
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ error: error }, { status: 500 })
+    }
+
 }
